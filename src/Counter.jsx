f10720f7e79adbcbff97ffd754d00bc495574607
jwkwon0817 <jeewon.kwon.0817@gmail.com>
2023-05-30 16:50:43 +0900

    Counter JSX

---
+++ src/Counter.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react';
+import { OddEvenResult } from './OddEvenResult';
+
+export const Counter = ({ initialValue }) => {
+	const [ count, setCount ] = useState(initialValue);
+	
+	const onIncrease = () => {
+		setCount(count + 1);
+	};
+	
+	const onDecrease = () => {
+		setCount(count - 1);
+	};
+	
+	return (
+		<div>
+			<h2>{ count }</h2>
+			<button onClick={ onIncrease }>+</button>
+			<button onClick={ onDecrease }>-</button>
+			<OddEvenResult count={ count } />
+		</div>
+	);
+};
+
+Counter.defaultProps = {
+	initialValue: 0,
+};
